90c1c42234309b48706a166a1cb63a125f729ce1
Luca Sideri <luca.sideri@outlook.com>
2019-05-27 22:38:52 +0200

    Added README and commenting

---
+++ README.md
@@ -0,0 +1,8 @@
+As the name sugggests, a simple script that uploads anonymously to imgur. The URL of the image gets copied directly into the user's clipboard.
+I made this script while learning python.  
+It uses the [Pyperclip](https://github.com/asweigart/pyperclip "Pyperclip GitHub") and [Requests](.python-requests.org/ "Requests website") modules.
+
+#### Feature wishlist:
+* Upload from url
+* GUI interface
+  - Drag and drop
--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
+# imports required modules
 import requests
-from random import randint
 import pyperclip
 
+# asks the user for a directory, then opens the file
+# in binary mode and return the result
 def read_file():
     print('Please insert image direcory:')
     dir = input('> ')
@@ -9,20 +11,24 @@ def read_file():
     file = open(dir, 'rb')
     return file.read()
 
+# sends the request to the imgur api and returns the link of the uploaded image
+# Accepts the image as an argoument
 def upload_image(i):
     print("Uploading...")
 
     url = 'https://api.imgur.com/3/image'
     payload = {'image': i}
-    files = {}
     headers = {
+    #The Client ID is bound to your application. You can create one at: https://api.imgur.com/oauth2/addclient
       'Authorization': 'Client-ID ec61be071b16841'
     }
 
-    response = requests.request('POST', url, headers = headers, data = payload, files = files)
+    response = requests.request('POST', url, headers = headers, data = payload)
     return response.json()['data']['link']
+
 
 image = read_file()
 link = upload_image(image)
+#copies the link in the clipboard
 pyperclip.copy(link)
 print(f"Upload complete.\nLink: {link}\nLink copied to clipboard.")
