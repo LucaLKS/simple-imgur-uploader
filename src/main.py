0df044183499832ce4ef439cf3c47f4f6b7b4113
Luca Sideri <luca.sideri@outlook.com>
2019-05-29 13:00:43 +0200

    Changed to use argparse

--- src/main.py
+++ src/main.py
@@ -1,25 +1,39 @@
 # imports required modules
 import requests
 import pyperclip
-from sys import argv
+import argparse
 
 
-# asks the user for a directory
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--directory', '-d', help="Directory of the image to upload.")
+    args = parser.parse_args()
+
+    if args.directory == None:  # If no directory is passed as argument calls the func
+        args.directory = prompt_user()
+
+    image = read_file(args.directory)
+    link = upload_image(image)
+    pyperclip.copy(link)  # copies the link in the clipboard
+    print(f"Upload complete.\nLink: {link}\nLink copied to clipboard.")
+
+
 def prompt_user():
+    """asks the user for a directory"""
     print('Please insert image direcory:')
     inpt = input('> ')
     return inpt
 
 
-# opens the file in binary mode and return the result
 def read_file(dir):
+    """opens the file in binary mode and return the result"""
     file = open(dir, 'rb')
     return file.read()
 
 
-# sends the request to the imgur api and returns the link of the uploaded image
-# accepts the image as an argoument
 def upload_image(i):
+    """sends the request to the imgur api and returns the link of the uploaded image
+    accepts the image as an argoument"""
     print("Uploading...")
 
     url = 'https://api.imgur.com/3/image'
@@ -34,13 +48,6 @@ def upload_image(i):
     return response.json()['data']['link']
 
 
-try:
-    script, directory = argv
-except ValueError:
-    directory = prompt_user()
-
-image = read_file(directory)
-link = upload_image(image)
-# copies the link in the clipboard
-pyperclip.copy(link)
-print(f"Upload complete.\nLink: {link}\nLink copied to clipboard.")
+if __name__ == '__main__':
+    """Works only if this isn't imported"""
+    main()
